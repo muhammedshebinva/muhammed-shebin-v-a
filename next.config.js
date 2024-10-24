9ca7837351ee18aab818770ff416f9d9d0c54814
twinksaji223@gmail.com <muhammedshebinva@gmail.com>
2024-10-24 23:54:11 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 
-// https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
+
 const nextConfig = {
   webpack: config => {
     const oneOfRule = config.module.rules.find(rule => rule.oneOf);
