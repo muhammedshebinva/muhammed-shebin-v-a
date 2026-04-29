c60035469b90179f8514e43f0f5ee2491f0f063a
Abina-Anto <abinaanto73@gmail.com>
2026-04-29 13:38:51 +0530

    images added

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -19,7 +19,7 @@ import porfolioImage5 from '../images/portfolio/olx clone.png';
 //import  heroImage from '../images/backgroundimg.png';
 import porfolioImage2 from '../images/portfolio/placementPortalHome.png';
 //import heroImage from '../images/header-background.webp';
-import heroImage from '../images/shebinIMGN1.jpg';
+// import heroImage from '../../public/images/';
 //import profilepic from '../images/profilepic.jpg';
 import profilepic from '../images/shebinimg1.jpg'
 import testimonialImage from '../images/testimonial.webp';
